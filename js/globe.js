b48d41557d51883e79dbb1eb380efa61db207975
Sriharish <sriharish.v@gmail.com>
2016-03-30 21:10:39 -0500

    Canvas Rendering (Phillip, Saif, Harish) with menus (Jorge)

--- js/globe.js
+++ js/globe.js
@@ -1,46 +1,96 @@
 //Control visibility of member functions with closure
 //Globe Renderer
-(function() {
-    
-    //Set Scene
-        var scene = new THREE.Scene();
+$( document ).ready(function() {
 
-        //Set Camera
-        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100);
-        camera.position.z = 10;
+    var webglEl = document.getElementById('globe');
+
+    if (!Detector.webgl) {
+        Detector.addGetWebGLMessage(webglEl);
+        return;
+    }
+
+    var width = window.innerWidth,
+		height = window.innerHeight;
+
+    // Earth params
+    var radius = 0.5,
+		segments = 32,
+		rotation = 6;
 
 
-        //Configure Render
-        var renderer = new THREE.WebGLRenderer({ antialias: true });
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        renderer.setClearColor(0xd1dbe3);
-        document.body.appendChild(renderer.domElement);
+    var scene = new THREE.Scene();
+
+    var camera = new THREE.PerspectiveCamera(65, width / height, 0.01, 100);
+    camera.position.z = 10;
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth * (.75), window.innerHeight * (.75));
+
+    scene.add(new THREE.AmbientLight(0x333333));
+
+    //var light = new THREE.DirectionalLight(0xffffff, 1);
+    //light.position.set(5, 3, 5);
+    //scene.add(light);
+
+    var sphere = createSphere(radius, segments);
+    sphere.rotation.y = rotation;
+    scene.add(sphere)
+
+    //var clouds = createClouds(radius, segments);
+    //clouds.rotation.y = rotation;
+    //scene.add(clouds)
+
+    //var stars = createStars(90, 64);
+    //scene.add(stars);
+
+    var controls = new THREE.TrackballControls(camera);
+
+    webglEl.appendChild(renderer.domElement);
+
+    render();
+
+    THREE.ImageUtils.crossOrigin = '';
+
+    function render() {
+        controls.update();
+        //sphere.rotation.y += 0.0005;
+        //clouds.rotation.y += 0.0005;
+        requestAnimationFrame(render);
+        renderer.render(scene, camera);
+    }
+
+    function createSphere(radius, segments) {
+        return new THREE.Mesh(
+			new THREE.SphereGeometry(radius, segments, segments),
+			new THREE.MeshPhongMaterial({
+			    map: THREE.ImageUtils.loadTexture('./images/2_no_clouds_4k.jpg'),
+			    //bumpMap: THREE.ImageUtils.loadTexture('./images/elev_bump_4k.jpg'),
+			    //bumpScale: 0.005,
+			    //specularMap: THREE.ImageUtils.loadTexture('./images/water_4k.png'),
+			    specular: new THREE.Color('grey')
+			})
+		);
+    }
+    /*
+    function createClouds(radius, segments) {
+        return new THREE.Mesh(
+			new THREE.SphereGeometry(radius + 0.003, segments, segments),
+			new THREE.MeshPhongMaterial({
+			    map: THREE.ImageUtils.loadTexture('./images/fair_clouds_4k.png'),
+			    transparent: true
+			})
+		);
+    }*/
+    /*
+    function createStars(radius, segments) {
+        return new THREE.Mesh(
+			new THREE.SphereGeometry(radius, segments, segments),
+			new THREE.MeshBasicMaterial({
+			    map: THREE.ImageUtils.loadTexture('./images/galaxy_starfield.png'),
+			    side: THREE.BackSide
+			})
+		);
+    }*/
 
 
-        //Configure Sphere
-        var geometry = new THREE.SphereGeometry(5, 32, 32, 6, 6.3, 6, 6.3);
-        var material = new THREE.Material({
-            transparent: true,
-            opacity: 0.5,
-            depthTest: true,
-            depthWrite: true,
-            alphaTest: 1,
-            side: THREE.FrontSide
-        });
-        var sphere = new THREE.Mesh(geometry, material);
-        var wireframe = new THREE.WireframeHelper(sphere, 0x00ffff);
-
-        scene.add(sphere);
-        scene.add(wireframe);
-
-        function render() {
-            requestAnimationFrame(render);
-            
-            sphere.rotation.y += 0.0001;
-            renderer.render(scene, camera);
-        }
-        render();
-    
-    
-})();
+});
